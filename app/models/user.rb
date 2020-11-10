ef117254a81a500202cca10f66dce613e17de1ee
soudatakeru <waito0202@gmail.com>
2020-11-10 14:34:05 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,22 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6}\z/
-    validates :password, presence: true,
-              format: { with: VALID_PASSWORD_REGEX }
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6}+\z/i
+    validates :password, format: { with: VALID_PASSWORD_REGEX }
   validates :nickname, presence: true
-  VALID_LAST_NAME =/^[ぁ-んァ-ヶー一-龠]+$/
-    validates :last_name:, presence: true,
-              format: { with: VALID_LAST_NAME }
-  VALID_FIRST_NAME =/^[ぁ-んァ-ヶー一-龠]+$/
-    validates :first_name, presence: true
-              format: { with: VALID_LAST_NAME }
-  VALID_LAST_NAME_KANA =/^[ア-ン゛゜ァ-ォャ-ョー「」、]+$/
-    validates :last_name_kana:, presence: true,
-              format: { with: VALID_LAST_NAME_KANA }
-  VALID_FIRST_NAME_KANA =/^[ア-ン゛゜ァ-ォャ-ョー「」、]+$/
-    validates :first_name_kana, presence: true
-              format: { with: VALID_LAST_NAME_KANA }
+  VALID_LAST_NAME = /\A[ぁ-んァ-ン一-龥]/
+    validates :last_name, format: { with: VALID_LAST_NAME }
+  VALID_FIRST_NAME = /\A[ぁ-んァ-ン一-龥]/
+    validates :first_name, format: { with: VALID_LAST_NAME }
+  VALID_LAST_NAME_KANA = /\A[ァ-ヶー－]+\z/
+    validates :last_name_kana, format: { with: VALID_LAST_NAME_KANA }
+  VALID_FIRST_NAME_KANA = /\A[ァ-ヶー－]+\z/
+    validates :first_name_kana, format: { with: VALID_LAST_NAME_KANA }
   validates :birthday, presence: true
      
 end
