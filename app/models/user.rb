7bbacb75f72a4ec25d914b02e0f36986740fe352
soudatakeru <waito0202@gmail.com>
2020-11-11 14:52:45 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i.freeze
-  validates :password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
-  validates :nickname, presence: true
-  VALID_LAST_NAME = /\A[ぁ-んァ-ン一-龥]/.freeze
-  validates :last_name, format: { with: VALID_LAST_NAME }
-  validates :first_name, format: { with: VALID_LAST_NAME }
-  VALID_LAST_NAME_KANA = /\A[ァ-ヶー－]+\z/.freeze
-  validates :last_name_kana, format: { with: VALID_LAST_NAME_KANA }
-  validates :first_name_kana, format: { with: VALID_LAST_NAME_KANA }
-  validates :birthday, presence: true
+  with_options presence: true do
+      validates :nickname
+      validates :birthday
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/.freeze } do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/.freeze } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+    with_options format: { with: /\A[a-z0-9]+\z/i.freeze } do
+      validates :password, length: { minimum: 6 }
+    end
+  end
 end
