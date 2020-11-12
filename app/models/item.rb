545255489a904a7bb80402e326a9080aa6ad08c2
soudatakeru <waito0202@gmail.com>
2020-11-12 11:20:48 +0900

    ActiveHash

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,37 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :condition
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :postage
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :handling
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  with_options presence: true do
+    validates :item
+    validates :name
+    validates :explanation
+    validates :category_id
+    validates :condition_id
+    validates :postage_payer_id
+    validates :prefecture_id
+    validates :handling_id
+    validates :price
+  end
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :postage_id
+    validates :handling_id
+    validates :prefecture_id
+  end
+
+    
+
 end
+
+
---
+++ spec/factories/handlings.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :handling do
+    
+  end
+end
