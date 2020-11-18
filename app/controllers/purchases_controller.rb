75805c78f75d9833f047876f7838062b089390dc
soudatakeru <waito0202@gmail.com>
2020-11-18 17:39:13 +0900

    購入商品保存機能

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,13 +1,30 @@
 class PurchasesController < ApplicationController
-
+  before_action :authenticate_user!, only: [:create]
   def index
     @item = Item.find(params[:item_id])
-    
     @purchase = UserPurchase.new
   end
 
   def create
-    binding.pry
+    @purchase = UserPurchase.new(user_purchase_params)
+    #unless @item.user_id == @item.user_id || if @items.present?
+      #redirect_to action: :index
+    #end
+    @item = Item.find(params[:item_id])
+    if @purchase.valid?
+      @purchase.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
+
+
+    private
+    def user_purchase_params
+      params.require(:user_purchase).permit(:purchase_id, :post_code, :city, :phone_number, :address, :prefecture_id, :building_name).merge(user_id: current_user.id, item_id: (params[:item_id]))
+    end
 end
+
+
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]/.freeze } do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/.freeze } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+    with_options format: { with: /\A[a-z0-9]+\z/i.freeze } do
+      validates :password, length: { minimum: 6 }
+    end
+  end
 end
