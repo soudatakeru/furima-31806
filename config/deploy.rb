0e2b6ad5d35006d936b85449710b9cccc0828f6a
soudatakeru <waito0202@gmail.com>
2020-11-23 18:05:25 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,8 @@ lock "~> 3.14.1"
 
 set :application, "furima-31806"
 set :repo_url,  'git@github.com:soudatakeru/furima-31806.git'
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :rbenv_type, :user
 set :rbenv_ruby, '2.6.5'
 set :ssh_options, auth_methods: ['publickey'],
                                   keys: ['~/.ssh/take3034.pem']
