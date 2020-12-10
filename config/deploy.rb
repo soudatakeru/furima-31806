f96d37caeab207b8932e9875034095c05b726b57
soudatakeru <waito0202@gmail.com>
2020-12-10 13:05:20 +0900

    a

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.14.1"
+lock "3.14.1"
 
 set :application, "furima-31806"
 set :repo_url,  'git@github.com:soudatakeru/furima-31806.git'
